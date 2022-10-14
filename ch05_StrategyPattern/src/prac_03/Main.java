247074f90f6881bfd0a4340a07f927ec42b4fc64
장정윤 <jeinie9179@naver.com>
2022-10-14 16:59:38 +0900

    StrategyPattern 실습 03

--- ch05_StrategyPattern/src/prac_03/Main.java
+++ ch05_StrategyPattern/src/prac_03/Main.java
@@ -4,16 +4,17 @@ import com.sun.security.jgss.GSSUtil;
 
 public class Main {
     public static void main(String[] args) {
-        Member member = new Member("kim", 10000);
+        Member member = new Member("kim", 1000);
         Member member1 = new Member("lee", 5000);
 
         Book book = new Book("title1", 10, 2000);
         Book book1 = new Book("title2", 5, 30000);
 
         Rental rental = new Rental(member, book, new BookDiscount());
-        Rental rental1 = new Rental(member1, book1, new MemberDiscount());
-
         rental.discountedState();
+        System.out.println();
+
+        Rental rental1 = new Rental(member1, book1, new MemberDiscount());
         rental1.discountedState();
 
         //member.buyBook(book); // 책 구매
