a870c70c5ce82f22623220cf6aee9253b948e380
장정윤 <jeinie9179@naver.com>
2022-10-24 11:11:30 +0900

    CommandPattern 실습

---
+++ week9/CommandPattern/Button.java
@@ -0,0 +1,12 @@
+public class Button {
+    private Command theCommand;
+    public Button(Command theCommand) {
+        this.theCommand = theCommand;
+    }
+    public void setCommand(Command newCommand) {
+        this.theCommand = newCommand;
+    }
+    public void pressed() {
+        theCommand.execute();
+    }
+}
