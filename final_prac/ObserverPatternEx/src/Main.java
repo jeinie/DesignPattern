f08034dc3d4224c82aa920b414cda15663a0720b
장정윤 <jeinie9179@naver.com>
2022-11-14 00:59:33 +0900

    Observer Pattern

--- final_prac/ObserverPatternEx/src/Main.java
+++ final_prac/ObserverPatternEx/src/Main.java
@@ -6,15 +6,16 @@ public class Main {
         Subscriber1 subscriber1 = new Subscriber1("김철수");
         Subscriber2 subscriber2 = new Subscriber2("김영희");
 
+        // 구독자 등록
         news.attach(subscriber1);
         news.attach(subscriber2);
 
-        String news_contents = "러시아-우크라이나 관련 뉴스입니다.";
-        news.notifyObservers(news_contents);
+        String news_contents = "러시아-우크라이나 관련 뉴스입니다."; // 뉴스 발간
+        news.notifyObservers(news_contents); // 구독자에게 알리기
 
         System.out.println("==================================");
 
-        news.detach(subscriber1);
+        news.detach(subscriber1); // 구독 취소
         news.notifyObservers(news_contents);
     }
 }
