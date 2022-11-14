36f4cc0309c11666d2a84704418120fc5c993b55
장정윤 <jeinie9179@naver.com>
2022-11-14 11:05:15 +0900

    Template Pattern (~ing)

---
+++ week12/src/Door.java
@@ -0,0 +1,19 @@
+
+public class Door {
+    private DoorStatus doorStatus;
+    public Door() {
+        doorStatus = DoorStatus.CLOSED;
+    }
+
+    public DoorStatus getDoorStatus() {
+        return doorStatus;
+    }
+
+    public void close() {
+        doorStatus = DoorStatus.CLOSED;
+    }
+
+    public void open() {
+        doorStatus = DoorStatus.OPENED;
+    }
+}
